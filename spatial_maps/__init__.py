08a603edba72fae100e7d343ccee919b078f4f53
Mikkel Elle Lepperød <lep.mik@gmail.com>
2019-04-01 15:06:04 +0200

    add functions to init

--- spatial_maps/__init__.py
+++ spatial_maps/__init__.py
@@ -1,7 +1,9 @@
 from .maps import SpatialMap
-from .gridcells import gridness, spacing_and_orientation
+from .gridcells import (
+    gridness, spacing_and_orientation, separate_fields_by_distance)
 from .fields import (
     border_score, calculate_field_centers, separate_fields_by_laplace)
 from .stats import (
     sparsity, selectivity, information_rate, information_specificity,
     prob_dist)
+from .tools import autocorrelation, fftcorrelate2d
