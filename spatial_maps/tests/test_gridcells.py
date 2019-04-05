d59e57b587d7b7e7c7930d935feb58c8aa268fd4
Mikkel Elle Lepperød <lep.mik@gmail.com>
2019-04-05 14:50:50 +0200

    testing

--- spatial_maps/tests/test_gridcells.py
+++ spatial_maps/tests/test_gridcells.py
@@ -2,7 +2,9 @@ import numpy as np
 import pytest
 from spatial_maps import SpatialMap
 import quantities as pq
-from spatial_maps.tools import make_test_grid_rate_map, make_test_spike_map
+from spatial_maps.tools import (
+    make_test_grid_rate_map, make_test_spike_map, autocorrelation)
+from spatial_maps.fields import find_peaks
 from spatial_maps.gridcells import (
     gridness, spacing_and_orientation, separate_fields_by_distance)
 
@@ -21,7 +23,7 @@ def test_gridness():
     assert round(g, 1) == 1.3
 
 
-def test_spacing_and_orientation():
+def test_spacing_and_orientation_from_true_peaks():
     box_size = np.array([1., 1.])
     rate = 5.
     bin_size = [.01, .01]
@@ -34,6 +36,25 @@ def test_spacing_and_orientation():
     spacing, orientation = spacing_and_orientation(pos_fields, box_size)
     assert spacing == spacing_true
     assert round(orientation * 180 / np.pi) == 30
+
+
+def test_spacing_and_orientation_from_autocorr():
+    box_size = np.array([1., 1.])
+    rate = 5.
+    bin_size = [.01, .01]
+    spacing_true = 0.3
+    orientation_true = .3
+
+    rate_map, pos_fields, xbins, ybins = make_test_grid_rate_map(
+        sigma=0.05, spacing=spacing_true, amplitude=rate, offset=0, box_size=box_size,
+        bin_size=bin_size, orientation=orientation_true)
+    autocorrelogram = autocorrelation(rate_map)
+    peaks = find_peaks(autocorrelogram)
+    real_peaks = peaks * bin_size
+    autocorrelogram_box_size = box_size * autocorrelogram.shape[0] / rate_map.shape[0]
+    spacing, orientation = spacing_and_orientation(real_peaks, autocorrelogram_box_size)
+    assert round(spacing, 1) == spacing_true
+    assert round(orientation, 1) == orientation_true
 
 
 def test_separate_fields_by_distance():
