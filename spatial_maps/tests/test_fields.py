3b8a09f09c8571e8989eb71e2d353cb54bc794b7
halvarsu <halvard.sutterud@gmail.com>
2019-06-25 15:27:33 +0200

    clean up, fix dist_func, make tests

--- spatial_maps/tests/test_fields.py
+++ spatial_maps/tests/test_fields.py
@@ -4,7 +4,7 @@ import quantities as pq
 from spatial_maps.tools import make_test_grid_rate_map, make_test_border_map
 from spatial_maps.fields import (
     separate_fields_by_laplace, find_peaks, calculate_field_centers,
-    border_score)
+    border_score, in_field, distance_to_edge_function, map_pass_to_unit_circle)
 
 
 def test_find_peaks():
@@ -56,3 +56,73 @@ def test_border_score():
     labels = separate_fields_by_laplace(rate_map, threshold=0)
     bs = border_score(rate_map, labels)
     assert round(bs, 2) == .32
+
+
+def test_in_field():
+    n_bins   = 10
+    box_size = 1
+    bin_size = box_size/n_bins
+
+    fields     = np.zeros((n_bins, n_bins))
+    fields[:5] = 1
+    fields[7]  = 2
+
+    # pick out center of bins
+    x = np.arange(bin_size/2, box_size, bin_size)
+    y = box_size/2*np.ones_like(x)
+    assert np.all(in_field(x,y,fields, box_size, box_size) 
+                  == [1,1,1,1,1,0,0,2,0,0])
+    # test edges
+    x = np.array([0,1])
+    y = np.array([0.5,0.5])
+    fields[:] = 0
+    fields[0] = 1
+    fields[-1] = 2
+    assert np.all(in_field(x,y,fields,box_size,box_size) == [1,2])
+
+
+def test_distance_to_edge_function():
+    from fields import distance_to_edge_function
+    n_bins   = 10
+    box_size = 1
+    bin_size = box_size/n_bins
+
+    field    = np.zeros((n_bins, n_bins))
+    field[2:8,2:8] = 1
+    d = distance_to_edge_function(0.5, 0.5, field,box_size, box_size,
+            interpolation = 'linear')
+
+    # assert edges 3/10 of the box size from center
+    for a in [i*np.pi/2 for i in range(4)]:
+        assert np.isclose(0.3, d(a))
+
+    # assert area within 5% of expected result
+    angles = np.linspace(0,2*np.pi,10000)
+    dist = d(angles)
+    x = dist*np.cos(angles)
+    y = dist*np.sin(angles)
+
+    dx = np.gradient(x)
+    dy = np.gradient(y)
+
+    # Greens theorem
+    area = 0.5 * np.sum(x*dy-y*dx)
+    exact_area = np.sum(field)/np.size(field)*box_size**2
+
+    assert np.abs(area-exact_area)/exact_area < 0.05
+
+def test_map_pass_to_unit_circle():
+    from fields import map_pass_to_unit_circle 
+
+    dist_func = lambda theta : 1
+    x_c, y_c = (0.5, 0.5)
+    theta = np.linspace(np.pi,2*np.pi,100)
+    t = theta
+    x = x_c + np.cos(theta)
+    y = y_c + np.sin(theta)
+
+    r,angle = map_pass_to_unit_circle(x,y,t,x_c,y_c,dist_func)
+    
+    assert np.all(np.isclose(angle, theta%(2*np.pi)))
+    assert np.all(np.isclose(1,r))
+
