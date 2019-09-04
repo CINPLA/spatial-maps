4041c85b7d3fbd55d357ce5ac1858b1bdd3489ba
halvarsu <halvard.sutterud@gmail.com>
2019-09-04 09:09:47 +0200

    fix __init__.py

--- spatial_maps/__init__.py
+++ spatial_maps/__init__.py
@@ -8,4 +8,3 @@ from .stats import (
     sparsity, selectivity, information_rate, information_specificity,
     prob_dist)
 from .tools import autocorrelation, fftcorrelate2d
-from .phase_precession import PassMask
