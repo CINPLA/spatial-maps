f520d646d8bf394710950356505dc8a0890717a2
Halvard Sutterud <halvard.sutterud@gmail.com>
2019-11-23 12:55:10 +0100

    Infield detector, compute distance to edge and map pass to unit circle (#13)
    * temporary folder with infield, theta and isi stuff
    
    warning, everything is broken and has to be translated into the new
    spatial-maps & cinpla-base -structure.
    
    * move in_field.py
    
    * update __init__.py
    
    * clean up, fix dist_func, make tests
    
    * remove plt
    
    * add crossings and passmask
    
    * remove passmask
    
    * fix __init__.py

--- spatial_maps/fields.py
+++ spatial_maps/fields.py
@@ -151,3 +151,156 @@ def border_score(rate_map, fields):
     d_m = np.average(dist_to_nearest_wall, weights=rate_map)
     b = (C_M - d_m) / (C_M + d_m)
     return b
+
+
+def in_field(x, y, field, box_size):
+    """Returns which spatial field each (x,y)-position is in. 
+
+    Parameters:
+    -----------
+    x : numpy array
+    y : numpy array, len(y) == len(x)
+    field : numpy nd array 
+        labeled fields, where each field is defined by an area separated by
+        zeros. The fields are labeled with indices from [1:].
+    box_size: list of two floats
+        extents of arena
+
+    Returns:
+    --------
+    indices : numpy array, length = len(x)
+        arraylike x and y with field-labeled indices
+    """
+
+    if len(x)!= len(y):
+        raise ValueError('x and y must have same length')
+
+    sx,sy   = field.shape
+    # bin sizes
+    dx      = box_size[0]/sx
+    dy      = box_size[1]/sy
+    x_bins  = dx + np.arange(0, box_size[0], dx) 
+    y_bins  = dy + np.arange(0, box_size[1], dy) 
+    ix      = np.digitize(x, x_bins) 
+    iy      = np.digitize(y, y_bins)
+
+    # fix for boundaries:
+    ix[ix==sx] = sx-1
+    iy[iy==sy] = sy-1
+    return np.array(field[ix,iy])
+
+
+def crossings(field_indices):
+    """indices at which a field is entered or exited"""
+    # +1 to enter at first time inside
+    cross = np.where(np.diff(field_indices))[0]+1
+
+    # add first and last time steps as 'boundary crossings', then drop duplicates
+    cross = np.unique(np.hstack([0, cross, field_indices.size-1]))
+    return cross
+
+
+def distance_to_edge_function(
+        x_c, y_c, field, box_xlen, box_ylen,interpolation = 'linear'):
+    """Returns a function which for a given angle returns the distance to
+    the edge of the field from the center.
+    Parameters:
+        x_c
+        y_c
+        field: numpy 2d array
+            ones at field bins, zero elsewhere
+    """
+
+    from skimage import measure
+    from scipy import interpolate
+    contours = measure.find_contours(field, 0.8) 
+    
+    box_dim = np.array([box_xlen, box_ylen])
+    edge_x, edge_y = (contours[0]*box_dim/(np.array(field.shape)-(1,1))).T 
+
+    # # angle between 0 and 2\pi
+    angles = np.arctan2((edge_y-y_c), (edge_x-x_c))%(2*np.pi) 
+    a_sort = np.argsort(angles)
+    angles = angles[a_sort]
+    edge_x = edge_x[a_sort]
+    edge_y = edge_y[a_sort]
+
+    # # Fill in edge values for the interpolation
+    pad_a = np.pad(angles, 2, mode='linear_ramp', end_values=(0,2*np.pi))
+    ev_x = (edge_x[0]+edge_x[-1])/2
+    pad_x = np.pad(edge_x, 2, mode = 'linear_ramp',
+            end_values=ev_x)
+    ev_y = (edge_y[0]+edge_y[-1])/2
+    pad_y = np.pad(edge_y, 2, mode = 'linear_ramp',
+            end_values=ev_y)
+
+    if interpolation=='cubic':
+        mask = np.where(np.diff(pad_a) == 0)
+        pad_a = np.delete(pad_a, mask)
+        pad_x = np.delete(pad_x, mask)
+        pad_y = np.delete(pad_y, mask)
+
+    x_func = interpolate.interp1d(pad_a,pad_x,kind=interpolation)
+    y_func = interpolate.interp1d(pad_a,pad_y,kind=interpolation)
+
+    def dist_func(angle):
+        x = x_func(angle)
+        y = y_func(angle)
+        dist = np.sqrt((x-x_c)**2 + (y-y_c)**2)
+        return dist
+
+    return dist_func
+
+
+def map_pass_to_unit_circle(x,y,t, x_c, y_c, dist_func):
+    """Uses three vectors {v,p,q} to map the passes to the unit circle. v
+    is the average velocity vector of the pass, p is the vector from the
+    position (x,y) to the center of the field and q is the vector from the
+    center to the edge through (x,y). See [1].
+    
+    Parameters:
+    -----------
+        :x, y, t: np arrays
+            should contain x,y and t data in numpy arrays
+        :x_c , y_c: floats
+            bump center
+        :field: np 2d array
+            bins indicating location of field.
+        :dist_func: function
+            dist_func(angle) = distance to bump edge from center
+        :return_vecs(optional): bool, default False
+
+    Returns:
+    --------
+        r : arrays of distance to origin on unit circle
+        theta : arrays of angles to axis defined by mean velocity vector
+
+    References:
+    -----------
+        [1]: A. Jeewajee et. al., Theta phase precession of grid and
+        placecell firing in open environment
+    """
+    pos = np.array((x,y))
+
+    # vector from pos to center p
+    p_vec = ((x_c,y_c) - pos.T).T 
+    # angle between x-axis and negative vector p
+    angle = (np.arctan2(p_vec[1],p_vec[0]) + np.pi)%(2*np.pi)
+    # distance from center to edge at each angle
+    q = dist_func(angle)
+    # distance from center to pos
+    p = np.linalg.norm(p_vec,axis=0)
+    # r-coordinate on unit circle
+    r = p/q
+
+    dpos = np.gradient(pos, axis = 1)
+    dt   = np.gradient(t)
+    vel  = np.divide(dpos,dt)
+
+    # mean velocity vector v
+    v_vec = np.average(vel,axis = 1)
+    # angle on unit circle, run is rotated such that mean velocity vector
+    # is toward positive x
+    theta = (angle - np.arctan2(v_vec[1],v_vec[0]))%(2*np.pi)
+    return r, theta
+
